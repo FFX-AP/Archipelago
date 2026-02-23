54559d49987035924049d99dc78fbf4e0f8e7a69
Andrew K <53620706+Andrewki44@users.noreply.github.com>
2026-02-23 22:34:26 +1100

    Update worlds/ffx/locations.py
    Co-authored-by: Omnises Nihilis <OmnisGamers@gmail.com>

--- worlds/ffx/locations.py
+++ worlds/ffx/locations.py
@@ -706,7 +706,7 @@ FFXTreasureLocations: List[FFXLocationData] = [ FFXLocationData(location[1]+Trea
     #("MUSH: Valley - Code VICTORIOUS",                                                             368, False),  # Gear: buki_get #72 [48h] { Rikku [06h], Armor {Lightningproof [8028h], Fireproof [8020h], Iceproof [8024h], Empty} } UNCOMMENT WHEN CODES ARE INCORPORATED
     #("BSIL: BSIL Ruins - Code MURASAME",                                                           369, False),  # Gear: buki_get #73 [49h] { Auron [02h], Weapon {Piercing [800Bh], One MP Cost [800Dh], Empty, Empty} } UNCOMMENT WHEN CODES ARE INCORPORATED
     ("CALM: Speed Sphere x30 (Lose Aeon Fight)",                                                    370, False),  # Item: 30x Speed Sphere [2048h]
-    ("CALM & REMI: Obtain the Aeon's Soul",                                                                       371, False),  # Key Item: Aeon's Soul [A01Fh]
+    ("CALM & REMI: Defeat Belgemine to obtain the Aeon's Soul",                                   371, False),  # Key Item: Aeon's Soul [A01Fh]
     ("MOON: South Bank Road - Fight Belgemine (Win) (Event)",                                       372, False),  # Item: 2x Dragon Scale [2021h]
     #("MOON: South Bank Road - Fight Belgemine (Lose) (Event)",                                     373, False),  # Item: 6x Smoke Bomb [2028h]
     ("MOON & REMI: Obtain the Summoner's Soul",                                                                   374, False),  # Key Item: Summoner's Soul [A01Eh]
